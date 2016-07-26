ee5be072dffed8a1045a3df98bbd79e08d9896ab
Fernando Souza <fernando.souza@liferay.com>
2016-07-26 18:30:25 -0300

    LPS-66783 SF removing settings modal methods

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_field_support.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_field_support.js
@@ -83,16 +83,6 @@ AUI.add(
 				return settings;
 			},
 
-			getSettingsModal: function() {
-				var instance = this;
-
-				var builder = instance.get('builder');
-
-				var settingsModal = builder._fieldSettingsModal;
-
-				return settingsModal;
-			},
-
 			isAdding: function() {
 				var instance = this;
 
