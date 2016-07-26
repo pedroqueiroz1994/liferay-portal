b448fc97ca76ca6d528c9cf7631ecf417f0e0945
Fernando Souza <fernando.souza@liferay.com>
2016-07-26 18:16:41 -0300

    LPS-66783 Fills down the form fields settings without access the DOM

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_field_support.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_builder_field_support.js
@@ -124,7 +124,18 @@ AUI.add(
 						function(context) {
 							var settingsForm = instance._createSettingsForm(context);
 
-							instance.updateSettingsFormValues(settingsForm);
+							var visitor = settingsForm.get('visitor');
+
+							visitor.set(
+								'fieldHandler',
+								function(formFieldContext) {
+									instance._fillSettingsFormField(formFieldContext, settingsForm);
+								}
+							);
+
+							visitor.visit();
+
+							settingsForm.set('context', context);
 
 							return settingsForm;
 						}
@@ -184,6 +195,28 @@ AUI.add(
 						templateNamespace: 'ddm.settings_form'
 					}
 				);
+			},
+
+			_fillSettingsFormField: function(formFieldContext, settingsForm) {
+				var instance = this;
+
+				var instanceContext = instance.get('context');
+
+				var contextKey = RendererUtil.getFieldNameFromQualifiedName(formFieldContext.name);
+
+				if (contextKey === 'name') {
+					var fieldName = instanceContext.fieldName;
+
+					if (!fieldName) {
+						fieldName = settingsForm._generateFieldName();
+					}
+
+					formFieldContext.value = fieldName;
+					instanceContext.fieldName = fieldName;
+				}
+				else if (contextKey in instanceContext) {
+					formFieldContext.value = instanceContext[contextKey];
+				}
 			},
 
 			_renderFormBuilderField: function() {
