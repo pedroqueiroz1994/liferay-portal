6a3a0a380f338a8f0da8602c6f68b768713f687f
Clovis Neto <clovis.neto@liferay.com>
2018-01-25 19:35:17 -0300

    LPS-76449 Append the feedback message to formGroup

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-editor/src/main/resources/META-INF/resources/editor_field.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-editor/src/main/resources/META-INF/resources/editor_field.js
@@ -98,7 +98,9 @@ AUI.add(
 										}
 									},
 									namespace: name,
+									onBlurMethod: A.bind(instance._afterBlur, instance),
 									onChangeMethod: A.bind(A.debounce(instance._onChangeEditor, 100), instance),
+									onFocusMethod: A.bind(instance._afterFocus, instance),
 									plugins: [],
 									textMode: false
 								}
@@ -109,7 +111,17 @@ AUI.add(
 
 						return instance;
 					},
-
+					
+					processEvaluationContext: function(context) {
+						var instance = this;
+						
+						if (!instance.hasFocus()) {
+							context.valid = true;
+						}
+						
+						return context;
+					},
+					
 					setValue: function(value) {
 						var instance = this;
 
